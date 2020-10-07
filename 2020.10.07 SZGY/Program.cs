75ced004616a845f8667705c18ba78b152614953
kecskegamer2001 <70634456+kecskegamer2001@users.noreply.github.com>
2020-10-07 10:00:29 +0200

    Add project files.

---
+++ 2020.10.07 SZGY/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2020._10._07_SZGY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
