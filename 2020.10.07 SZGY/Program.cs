c0ac5d0c4dc25d2c29cf57351c91f935892cdac1
kecskegamer2001 <70634456+kecskegamer2001@users.noreply.github.com>
2020-10-07 11:11:17 +0200

    asd

--- 2020.10.07 SZGY/Program.cs
+++ 2020.10.07 SZGY/Program.cs
@@ -9,18 +9,19 @@ namespace _2020._10._07_SZGY
     class Program
     {
         static int[] tomb = new int[100];
-
+        static Random rmd = new Random();
         static void Main(string[] args)
         {
             feltolt();
             kiir();
+            Console.ReadKey();
+            RandomHelyreIr();
             Console.ReadKey();
             
         }
         static void feltolt()
         {
             
-            Random rmd = new Random();
             for (int i = 0; i < tomb.Length; i++)
             {
                 tomb[i] = rmd.Next(10,100);
@@ -43,6 +44,10 @@ namespace _2020._10._07_SZGY
         }
         static void RandomHelyreIr()
         {
+            int x = rmd.Next(0,10);
+            int y = rmd.Next(0,10);
+            Console.SetCursorPosition(x,y);
+            Console.Write("*");
 
         }
         
