795b7b01e71a4cdd23c1639e9f6a45d6aaff66f3
kecskegamer2001 <70634456+kecskegamer2001@users.noreply.github.com>
2020-10-07 10:56:25 +0200

    sggdsfg

--- 2020.10.07 SZGY/Program.cs
+++ 2020.10.07 SZGY/Program.cs
@@ -8,8 +8,36 @@ namespace _2020._10._07_SZGY
 {
     class Program
     {
+        static int[] tomb = new int[100];
+
         static void Main(string[] args)
         {
+            feltolt();
+            kiir();
+            Console.ReadLine();
+            
         }
+        static void feltolt()
+        {
+            
+            Random rmd = new Random();
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = rmd.Next(10,100);
+                Console.WriteLine(tomb[i]);
+            }
+        }
+        static void kiir()
+        {
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                Console.WriteLine(tomb[i]);
+
+            }
+        }
+        
+        }
+        
     }
 }
